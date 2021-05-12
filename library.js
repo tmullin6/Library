e1c0449df7fea7b14b37a2bf6aa8b4abbcfe62a0
tmullin6 <tyleramullins@gmail.com>
2021-05-11 18:21:53 -0600

    Added functionality to add book to library

--- library.js
+++ library.js
@@ -1,35 +1,83 @@
 let myLibrary =[];
 
+const library = document.querySelector(".library");
+const add = document.querySelector("#add");
 
-let book =Object.create(Book);
-let book2=Object.create(Book);
 
-book.title="The Hobbit";
-book.author="JRR Tolkein";
-book.length=286;
-book.isRead=true;
 
-book2.title="a";
-book2.author="b";
-book2.length=100;
-book2.isRead=false;
+add.addEventListener("click", ()=>{
 
-addBook(book);
-addBook(book2);
+    let book = Object.create(Book);
+    book.title=prompt("Title");
+    book.author=prompt("Author");
+    book.pages=Number.parseInt(prompt("Length"));
+    let read=prompt("Have you read it Y/N");
 
-console.log(myLibrary);
+    if (read=="Y"|| read=="y") {
+        book.isRead=true;
+    }
+    else {
+        book.isRead=false;
+    }
 
-function Book(title,author,length,isRead) {
+    addBook(book);
+    
+    displayLibrary(myLibrary);
+
+});
+
+
+
+function Book(title,author,pages,isRead) {      //Book Object Constructor
 
     this.title=title;
     this.author-author;
-    this.length=length;
+    this.pages=pages;
     this.isRead=isRead;
 
 };
+
+
 
 function addBook(Book) {
 
     myLibrary.push(Book);
     return myLibrary;
+};
+
+
+
+function displayLibrary (arr) {
+
+    for (let i = 0; i<arr.length; i++) {
+
+        const card = document.createElement("div");
+        const title= document.createElement("p");
+        const author= document.createElement("p");
+        const pages= document.createElement("p");
+        const isRead= document.createElement("p");
+        const remove = document.createElement("button");
+
+        card.classList.add("card");
+        
+        title.textContent=arr[i].title;
+        author.textContent=arr[i].author;
+        pages.textContent=arr[i].pages;
+
+        if (arr[i].isRead == true) {
+        isRead.textContent="Have read";
+        }
+        else{
+            isRead.textContent ="Have not read";
+        }
+
+        remove.textContent="Remove Book";
+
+        library.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(isRead);
+        card.appendChild(remove);
+    }
 }
